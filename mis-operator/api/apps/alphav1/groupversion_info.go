19a02efd83338129d632a1915b6da82b1993757c
developer <developer@com>
2025-08-26 16:50:35 +0800

    Match-id-e42b56907244b6000c668a07fbd493b5b17a0def

--- mis-operator/api/apps/alphav1/groupversion_info.go
+++ mis-operator/api/apps/alphav1/groupversion_info.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "apps.ascend.com", Version: "alphav1"}
+	GroupVersion = schema.GroupVersion{Group: "mis.hiascend.com", Version: "alphav1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
